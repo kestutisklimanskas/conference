a8722cc8f030fa586827a6575b5f9358c297f1f3
Kęstutis Klimanskas <kestutis.klimanskas@stud.vilniustech.lt>
2024-04-21 23:12:02 +0300

    Login AUTH created, Registration, Logout, Tailwind CSS added, couple of Javascript scripts added for logout, CSS styling, login, register views, conference viewing for guest users and minor fixes

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -4,8 +4,122 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\User;
+use resources\views\conferences\index;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 class ApiController extends Controller
 {
-    //
+    public function showRegistrationForm()
+    {
+        return view('register');
+    }
+
+    public function showLoginForm()
+    {
+        return view('login');
+    }
+    public function register(Request $request)
+    {
+        try{
+        $validateUser = Validator::make($request->all(),[
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users, email',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        if($validateUser->fails()){
+            return response()->json($validateUser->errors()->toJson(), 400);
+        }
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        return view('login');
+
+        // return response()->json([
+        //     'status' => true,
+        //     'message' => 'User successfully registered',
+        //     'user' => $user,
+        //     'token' => $user->createToken('API TOKEN')->plainTextToken,
+        // ], 201);
+
+
+     } catch(\Exception $e){
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation failedas',
+                'error' => $e->getMessage(),
+            ], 422);
+        }
+    }
+    public function login(Request $request)
+    {
+        try{
+        $validateUser = Validator::make($request->all(),[
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if($validateUser->fails()){
+            return response()->json($validateUser->errors()->toJson(), 400);
+        }
+
+        if(!Auth::attempt($request->only('email', 'password'))){
+            Session::flash('login_failed', 'Invalid email or password.');
+            
+        }
+
+        $user = Auth::user();
+
+        return redirect()->route('conferences.index');
+        
+    } catch(\Exception $e){
+        return response()->json([
+            'status' => false,
+            'message' => 'Validation failed',
+            'error' => $e->getMessage(),
+        ], 422);
+    }
+    }   
+    public function profile(Request $request)
+    {
+        $userData= auth()->user();
+        return response()->json([
+            'status' => true,
+            'message' => 'User profile',
+            'user' => $request->user(),
+            'data' => $userData,
+            'id' => auth()->user()->id,
+        ], 200);
+    }
+    public function logout(Request $request)
+    {
+        $userData= auth()->user();
+        $accessToken = $userData->currentAccessToken();
+        if ($request->user() && $request->user()->currentAccessToken()) {
+            $accessToken = $userData->currentAccessToken();
+            $accessToken->delete();
+        }
+    
+        //return view('login');
+      
+        // return response()->json([
+        //     'status' => true,
+        //     'message' => 'User successfully logged out',
+        //     'data' => $accessToken,
+            
+        // ], 200);
+
+        if(Auth::check()){
+            Auth::logout();
+            return redirect()->route('login');
+        }
+        //return redirect()->route('login');
+
+    }
 }
--- resources/views/conferences/layout.blade.php
+++ resources/views/conferences/layout.blade.php
@@ -1,21 +1,36 @@
-<!-- resources/views/layouts/app.blade.php -->
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
+  
     <title>Laravel app</title>
-    <link href = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel = "stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}"></script>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 </head>
-    <body>
-        @section('sidebar')
-            This is the master sidebar.
-        @show
- <h1>Laravel app</h1>
-        <div class="container">
-            @yield('content')
-        </div>
+<body>
+    
+    <h1><a href="{{ route('conferences.index') }}">Laravel app</a></h1>
+    <div class="container" id="auth-container">
+        @auth
+            <p>Logged in as {{ auth()->user()->name }}</p>
+            <form method="POST" action="{{ route('logout') }}">
+    @csrf
+    <button type="submit" class="btn btn-primary" id="logout-btn">Logout</button>
+</form>
+        @else
+            <p>You are a Guest</p>
+            <a id="loginButton" href="{{ route('login') }}" class="btn btn-primary">Login</a>
+            <a id="registerButton" href="{{ route('register') }}" class="btn btn-secondary">Register</a>
+        @endauth
+        
+    </div>
+    <div class="container">
+        @yield('content')
+    </div>
+  
 
-    </body>
+</body>
 </html>
